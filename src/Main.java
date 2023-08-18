e89ea365bbd1f560b1ec9f5926b2f89f886b4962
xiaoming <hello@xiaoming.cn>
2023-08-18 16:39:06 +0800

    从GitHub克隆项目后修改提交

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         int a = 1;
         int b = 2;
         int c = a+b;
+
+        System.out.println("从GitHub克隆项目后修改提交");
+
         System.out.println("将idea项目提交到GitHub");
         System.out.println(c);
         System.out.println("测试");
