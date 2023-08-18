150cec5308297d2520e0d89b69a7ddfdd66a76f2
xiaoming <hello@xiaoming.cn>
2023-08-18 15:52:15 +0800

    修改内容再次提交

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        int c = a+b;
+        System.out.println("将idea项目提交到GitHub");
+        System.out.println(c);
         System.out.println("测试");
         System.out.println("Hello World!");
     }
